15f5d1df73055db4216f989ec79a8d77e855091c
Maia Kamyab <mahtakamyab@gmail.com>
2017-10-14 14:11:17 -0700

    add skeleton for login page

--- code/frontend/src/components/Layout/index.js
+++ code/frontend/src/components/Layout/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Home from '../Home';
 import Menu from '../Menu';
+import Login from '../Login';
 import css from './layout.css';
 
 const mapStateToProps = (state) => {
   return {
-    tab: state.main.tab
-  }
+    tab: state.main.tab,
+    isLoggedIn: state.user.isLoggedIn
+  };
 };
 
-function LayoutContainer({tab}) {
+function LayoutContainer({tab, isLoggedIn}) {
+  if (!isLoggedIn) {
+    return (
+      <div className={css.wrapper}>
+        <Login />
+      </div>
+    );
+  }
   let child = null;
   switch (tab) {
     case 'home':
       child = <Home />;
       break;
     default:
-      child = null;
+      child = null; // TODO: replace with error page
       break;
   }
   return (
@@ -28,6 +38,11 @@ function LayoutContainer({tab}) {
       </div>
     </div>);
 }
+
+LayoutContainer.propTypes = {
+  tab: PropTypes.string.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired
+};
 
 const Layout = connect(
   mapStateToProps,
---
+++ code/frontend/src/components/Login/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUser, switchView } from '../../actions';
+import LoginComponent from './Login.jsx';
+
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSubmit: (username, password) => {
+      dispatch(loginUser(username, password));
+      dispatch(switchView('home')); //todo: needs to be dynamic
+    }
+  };
+};
+
+class LoginContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleUsernameInput = this.handleUsernameInput.bind(this);
+    this.handlePasswordInput = this.handlePasswordInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUsernameInput(event) {
+    event.preventDefault();
+    const {password} = this.state;
+    this.setState({
+      username: event.target.value,
+      password
+    });
+  }
+
+  handlePasswordInput(event) {
+    event.preventDefault();
+    const {username} = this.state;
+    this.setState({
+      username,
+      password: event.target.value
+    });
+  }
+
+  handleSubmit() {
+    const {state} = this;
+    this.props.onSubmit(state.username, state.password);
+  }
+
+  render() {
+    const {state} = this;
+    return <LoginComponent
+      username={state.username}
+      password={state.password}
+      handleUsernameInput={this.handleUsernameInput}
+      handlePasswordInput={this.handlePasswordInput}
+      handleSubmit={this.handleSubmit} />;
+  }
+}
+
+LoginContainer.propTypes = {
+  user: PropTypes.object.isRequired,
+  onSubmit: PropTypes.func.isRequired
+};
+
+const Login = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginContainer);
+
+export default Login;
