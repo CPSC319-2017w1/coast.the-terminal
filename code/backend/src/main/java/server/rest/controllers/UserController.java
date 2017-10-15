c3f8854685a891b511b2af1e1ec41fcffd1329d7
vaastav <vaastav.anand05@gmail.com>
2017-10-15 11:16:03 -0700

    Add functionality to authenticate login
    Also make changes to DatabaseConnection class and the way SQLExceptions
    are handled

--- code/backend/src/main/java/server/rest/controllers/UserController.java
+++ code/backend/src/main/java/server/rest/controllers/UserController.java
@@ -1,11 +1,13 @@
 package server.rest.controllers;
 
 import org.springframework.web.bind.annotation.RequestParam;
-import server.database.DatabaseConnection;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import server.database.DatabaseConnection;
 import server.rest.responses.LoginResponse;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,28 +18,37 @@ public class UserController {
     private static final String dbConnectionUrl = "jdbc:mysql://the-terminal-db-instance.c8lixxetvm6e.us-west-2.rds.amazonaws.com:3306/coast_capital_db";
     private static final String dbUsername = "Administrator";
     private static final String dbPassword = "TheTerminal!";
+    private static final String loginQuery = "select * from users where username=? and password=?";
+
     @RequestMapping("/login")
     public LoginResponse login(@RequestParam("username") String username,
                                @RequestParam("password") String password) {
+        DatabaseConnection connection = new DatabaseConnection(dbConnectionUrl, dbUsername, dbPassword);
+        boolean success = true;
+        String user = username;
+        String permissions = "none";
         try {
-            DatabaseConnection connection = new DatabaseConnection(dbConnectionUrl, dbUsername, dbPassword);
+            connection.openConnection();
+            if (!connection.isConnected()) {
+                return LoginResponse.loginFailure("Failed to open database connection");
+            }
+            PreparedStatement st = connection.getPreparedStatement(loginQuery);
+            st.setString(1, username);
+            st.setString(2, password);
+            ResultSet resultSet = st.executeQuery();
+            if (!resultSet.isBeforeFirst()) {
+                return LoginResponse.loginFailure("Incorrect username or password");
+            }
+            user = resultSet.getString("username");
+            permissions = resultSet.getString("permissions");
 
-            //TODO  do something with db connection to verify login credentials
-
-            boolean success = true;
-            String user = username;
-            String permissions = "none";
-
-            //NOTE Spring automatically converts all fields with getters into JSON for transmission
-            return new LoginResponse(user, success, permissions);
-
-        } catch(SQLException e) {
+            connection.closeConnection();
+        } catch (SQLException e) {
             Logger logger = Logger.getAnonymousLogger();
-            logger.log(Level.SEVERE, "Database connection failed: " + e.getMessage());
-            LoginResponse response = new LoginResponse("", false, "none");
-            response.setError(true);
-            response.setErrorMessage("Failed to connect to database");
-            return response;
+            logger.log(Level.INFO, "Login failed: " + e.getMessage());
         }
+
+        //NOTE Spring automatically converts all fields with getters into JSON for transmission
+        return new LoginResponse(user, success, permissions);
     }
 }
