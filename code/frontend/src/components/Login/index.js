569627c5874e3b0bf1bb32d5e12b115db26635c2
Shrey Swades Nayak <sswades@gmail.com>
2017-10-18 15:44:27 -0700

    no message

--- code/frontend/src/components/Login/index.js
+++ code/frontend/src/components/Login/index.js
@@ -56,13 +56,12 @@ class LoginContainer extends React.Component {
 
   render() {
     const {state} = this;
-    let username = state.username;
-    let password = state.password;
-    let handleUsernameInput = this.handleUsernameInput();
-    let handlePasswordInput = this.handlePasswordInput();
-    let handleSubmit = this.handleSubmit();
-
-    return LoginComponent({username, password, handleUsernameInput, handlePasswordInput, handleSubmit});
+      return <LoginComponent
+          username={state.username}
+          password={state.password}
+          handleUsernameInput={this.handleUsernameInput}
+          handlePasswordInput={this.handlePasswordInput}
+          handleSubmit={this.handleSubmit} />;
   }
 }
 
