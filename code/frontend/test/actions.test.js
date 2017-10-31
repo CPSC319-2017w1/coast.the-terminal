935ffb92ff1ff14dafa50891c5685bf61ea92803
Maia Kamyab <mahtakamyab@gmail.com>
2017-10-31 13:46:19 -0700

    add example action test

--- code/frontend/test/actions.test.js
+++ code/frontend/test/actions.test.js
@@ -1,11 +1,20 @@
 import {expect} from 'chai';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import * as actions from '../src/actions';
+import * as TYPES from '../src/constants/action-types.js';
 
-describe('Blobs', function() {
-  it('should list ALL blobs on /blobs GET');
-});
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
-describe('Test mocha', function() {
-  it('Should pass test', function() {
-    expect({a: 1}).to.not.have.property('b');
+describe('Login Successful', () => {
+  it('Should pass correct admin and username', () => {
+    const loginResponse = actions.loginSuccessful('user', true);
+    expect(loginResponse).to.have.property('username');
+    expect(loginResponse).to.have.property('isAdmin');
+    expect(loginResponse).to.have.property('type');
+    expect(loginResponse.isAdmin).to.be.true;
+    expect(loginResponse.username).to.equal('user');
+    expect(loginResponse.type).to.equal(TYPES.LOGIN);
   });
 });
