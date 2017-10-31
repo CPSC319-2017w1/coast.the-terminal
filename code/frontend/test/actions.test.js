ae8e13fc7a019fbe5305cfbdc3830b0f7398a9f9
Maia Kamyab <mahtakamyab@gmail.com>
2017-10-31 13:28:57 -0700

    setup mocha

---
+++ code/frontend/test/actions.test.js
@@ -0,0 +1,12 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Blobs', function() {
+  it('should list ALL blobs on /blobs GET');
+});
+
+describe('Test mocha', function() {
+  it('Should pass test', function() {
+    expect({a: 1}).to.not.have.property('b');
+  });
+});
