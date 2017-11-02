4bffa4b9090010efe4e501fb1ff3f5abe2f4664f
Shrey Swades Nayak <sswades@gmail.com>
2017-11-02 12:45:07 -0700

    Edited index.js of add contractor (incomplete)

--- code/frontend/src/components/ContractorInfo/index.js
+++ code/frontend/src/components/ContractorInfo/index.js
@@ -1,7 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import AddContractorComponent from './AddContractor.jsx';
 
-function ContractorInfo() {
-  return <div>Welcome to the Contractor Info</div>;
+const mapStateToProps = state => {
+    return {
+        user: state.user,
+        tab: state.main.tab
+    };
+};
+
+class AddContractorContainer extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstname: '',
+            lastname: '',
+            company: '',
+            projectname: '',
+            reportingmanager: '',
+            hrposition: '',
+            ratetype: '',
+            hourlyrate: '',
+            paygrade: '',
+            refno: '',
+        };
+        this.handleTextInput() = this.handleTextInput().bind(this);
+        this.handleDropdownInput() = this.handleDropdownInput().bind(this);
+        this.handleStatusInput() = this.handleStatusInput().bind(this);
+        this.handleCurrencyInput() = this.handleCurrencyInput().bind(this);
+        this.handleAdd() = this.handleAdd().bind(this);
+        this.handleSubmit() = this.handleSubmit().bind(this);
+    }
+    handleTextInput(){
+
+    }
+
+    handleDropdownInput(){
+
+    }
+
+    handleStatusInput(){
+
+    }
+
+    handleCurrencyInput(){
+
+    }
+
+    handleAdd(){
+
+    }
+
+    handleSubmit(){
+
+    }
+
+    render() {
+        return <AddContractorComponent/>;
+    }
 }
 
-export default ContractorInfo;
+const AddContractor = connect(
+    mapStateToProps,
+    null
+)(AddContractorContainer);
+
+export default AddContractor;
