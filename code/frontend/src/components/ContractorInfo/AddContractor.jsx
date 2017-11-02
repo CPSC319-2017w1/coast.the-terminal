fd688a2c5798be094fc40ceeb49464da3a47acf5
Shrey Swades Nayak <sswades@gmail.com>
2017-11-02 16:36:01 -0700

    updated add contractor

--- code/frontend/src/components/ContractorInfo/AddContractor.jsx
+++ code/frontend/src/components/ContractorInfo/AddContractor.jsx
@@ -1,63 +1,93 @@
 import React from 'react';
+import Dropdown from 'react-dropdown';
 import PropTypes from 'prop-types';
 import css from './addcontractor.css';
 
-function AddContractorComponent({handleTextInput, firstname, lastname, company, handleStatusInput,
-    handleDropdownInput, projectname, reportingmanager, hrposition, ratetype, hourlyrate, paygrade, refno, handleCurrencyInput,
-    handleAdd, handleSubmit, onClick}) {
-    return (
-        <div className={css.wrapper}>
-            <h1>Contractor Information</h1>
-            <p>Use the form below to add contractor information into the system.</p>
-            <div className={css.contractor-info}>
-                <form>
-                    First Name <input className={css.txtfield} type="text" value={firstname} onChange={handleTextInput}/>
-                    Last Name <input className={css.txtfield} type="text" value={lastname} onChange={handleTextInput}/>
-                    Company <input className={css.txtfield} type="text" value={company} onChange={handleTextInput}/>
-                    Status <input className={css.radiobutton} type="radio" onChange={handleStatusInput("active")}/> Active
-                    <input className={css.radiobutton} type="radio" onChange={handleStatusInput("inactive")}/> Inactive
-                </form>
-            </div>
-            <div className={css.project-info}>
-                <form>
-                    Project Name <input className={css.txtfield} type="text" value={projectname} onChange={handleTextInput}/>
-                    Reporting Manager <Dropdown className={css.txtfield} list = {} value={reportingmanager} onChange={handleDropdownInput}/>
+const fields = [
+  {
+    text: 'First Name',
+    key: 'firstname'
+  }
+];
+
+function getField(data, contractor) {
+  return <p>
+    {data.text}
+    <input className="" value={contractor[data.key]}/>
+  </p>;
+}
+
+function AddContractorComponent({Contractor, Projects, handleTextInput, handleStatusInput,
+  handleDropdownInput, handleCurrencyInput,
+  handleAdd, handleSubmit}) {
+  return (
+    <div className={css.wrapper}>
+      <h1>Contractor Information</h1>
+      <p>Use the form below to add contractor information into the system.</p>
+      <div className={css.contractorInfo}>
+        <form>
+          {
+            fields.map((field) => { return getField(field, Contractor); })
+          }
+          <p>
+                    First Name
+            <input className={css.txtfield}
+              name=""
+              type="text"
+              value={Contractor.firstname}
+              onChange={handleTextInput}/>
+          </p>
+          <p>
+                    Last Name
+            <input className={css.txtfield}
+              type="text" value={Contractor.lastname}
+              onChange={handleTextInput}/>
+          </p>
+          <p>
+                    Company
+            <input className={css.txtfield}
+              type="text"
+              value={Contractor.company}
+              onChange={handleTextInput}/>
+          </p>
+          <p>
+                    Status
+            <input className={css.radiobutton}
+              type="radio"
+              onChange={handleStatusInput('active')}/>
+                    Active
+            <input className={css.radiobutton}
+              type="radio"
+              onChange={handleStatusInput('inactive')}/>
+                    Inactive
+          </p>
+        </form>
+      </div>
+      <div className={css.projectInfo}>
+        <form>
+                    Project Name <input className={css.txtfield} type="text" value={Projects[0].projectname} onChange={handleTextInput}/>
+                    Reporting Manager <Dropdown className={css.txtfield} list = {Projects[0].reportingmanagers} onChange={handleDropdownInput}/>
                     Start Date
                     End Date
-                    {/*Use react JS datepicker or google other alternatives*/}
-                    HR Position <Dropdown className={css.txtfield} list = {} value={hrposition} onChange={handleDropdownInput}/>
-                    Rate Type <Dropdown className={css.txtfield} list = {} value={ratetype} onChange={handleDropdownInput}/>
-                    Est. Hourly Rate <input className={css.txtfield} type="text" value={hourlyrate} onChange={handleTextInput}/>
-                    HR Pay Grade <Dropdown className={css.txtfield} list = {} value={paygrade} onChange={handleDropdownInput}/>
-                    PO Reference Number <Dropdown className={css.txtfield} list = {} value={refno} onChange={handleDropdownInput}/>
+          {/*Use react JS datepicker or google other alternatives*/}
+                    HR Position <Dropdown className={css.txtfield} list = {Projects[0].hrpositions} onChange={handleDropdownInput}/>
+                    Rate Type <Dropdown className={css.txtfield} list = {Projects[0].ratetype}  onChange={handleDropdownInput}/>
+                    Est. Hourly Rate <input className={css.txtfield} type="text" value={Projects[0].hourlyrate} onChange={handleTextInput}/>
+                    HR Pay Grade <Dropdown className={css.txtfield} list = {Projects[0].paygrade}  onChange={handleDropdownInput}/>
+                    PO Reference Number <Dropdown className={css.txtfield} list={Projects[0].refno} onChange={handleDropdownInput}/>
                     Currency <input className={css.radiobutton} type="radio" onChange={handleCurrencyInput}/> USD
-                    <input className={css.radiobutton} type="radio" onChange={handleCurrencyInput}/> CAD
-                </form>
-            </div>
-            <input className={css.btnstyle} type="submit" onClick={handleAdd} value="Add Additional Contract" />
-            <input className={css.btnstyle} type="submit" onClick={handleSubmit} value="Add Contract" />
-        </div>
+          <input className={css.radiobutton} type="radio" onChange={handleCurrencyInput}/> CAD
+        </form>
+      </div>
+      <input className={css.btnstyle} type="submit" onClick={handleAdd} value="Add Additional Contract" />
+      <input className={css.btnstyle} type="submit" onClick={handleSubmit} value="Add Contract" />
+    </div>
 
-    );
+  );
 }
 
 AddContractorComponent.propTypes = {
-    handleTextInput: PropTypes.func.isRequired,
-    firstname: PropTypes.string.isRequired,
-    lastname: PropTypes.string.isRequired,
-    company: PropTypes.string.isRequired,
-    handleStatusInput: PropTypes.func.isRequired,
-    handleDropdownInput: PropTypes.func.isRequired,
-    projectname: PropTypes.string.isRequired,
-    reportingmanager: PropTypes.string.isRequired,
-    hrposition: PropTypes.string.isRequired,
-    ratetype: PropTypes.string.isRequired,
-    hourlyrate: PropTypes.string.isRequired,
-    paygrade: PropTypes.string.isRequired,
-    refno: PropTypes.string.isRequired,
-    handleCurrencyInput: PropTypes.func.isRequired,
-    handleAdd: PropTypes.func.isRequired,
-    handleSubmit: PropTypes.func.isRequired
+
 
 };
 
--- code/frontend/src/components/ContractorInfo/index.js
+++ code/frontend/src/components/ContractorInfo/index.js
@@ -4,66 +4,84 @@ import { connect } from 'react-redux';
 import AddContractorComponent from './AddContractor.jsx';
 
 const mapStateToProps = state => {
-    return {
-        user: state.user,
-        tab: state.main.tab
-    };
+  return {
+    user: state.user,
+    tab: state.main.tab
+  };
 };
 
+
+
 class AddContractorContainer extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            firstname: '',
-            lastname: '',
-            company: '',
-            projectname: '',
-            reportingmanager: '',
-            hrposition: '',
-            ratetype: '',
-            hourlyrate: '',
-            paygrade: '',
-            refno: '',
-        };
-        this.handleTextInput() = this.handleTextInput().bind(this);
-        this.handleDropdownInput() = this.handleDropdownInput().bind(this);
-        this.handleStatusInput() = this.handleStatusInput().bind(this);
-        this.handleCurrencyInput() = this.handleCurrencyInput().bind(this);
-        this.handleAdd() = this.handleAdd().bind(this);
-        this.handleSubmit() = this.handleSubmit().bind(this);
-    }
-    handleTextInput(){
+  constructor(props) {
+    super(props);
+    this.state = {
+      Contractor:{
+        firstname: '',
+        lastname: '',
+        company: ''
+      },
+      Projects: [
+        {
+          projectname: '',
+          reportingmanagers: ['harry potter', 'luna luvgood'],
+          hrpositions: ['professor', 'dark arts teacher'],
+          ratetype: '',
+          hourlyrate: '',
+          paygrade: '',
+          refno: ''
+        }
+      ]
 
-    }
+    };
+    this.handleTextInput = this.handleTextInput.bind(this);
+    this.handleDropdownInput = this.handleDropdownInput.bind(this);
+    this.handleStatusInput = this.handleStatusInput.bind(this);
+    this.handleCurrencyInput = this.handleCurrencyInput.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleTextInput(){
 
-    handleDropdownInput(){
+  }
 
-    }
+  handleDropdownInput(){
 
-    handleStatusInput(){
+  }
 
-    }
+  handleStatusInput(){
 
-    handleCurrencyInput(){
+  }
 
-    }
+  handleCurrencyInput(){
 
-    handleAdd(){
+  }
 
-    }
+  handleAdd(){
 
-    handleSubmit(){
+  }
 
-    }
+  handleSubmit(){
 
-    render() {
-        return <AddContractorComponent/>;
-    }
+  }
+
+  render() {
+    return <AddContractorComponent
+      Contractor={this.state.Contractor}
+      Projects={this.state.Projects}
+      handleTextInput={this.handleTextInput}
+      handleStatusInput={this.handleStatusInput}
+      handleDropdownInput={this.handleDropdownInput}
+      handleCurrencyInput={this.handleCurrencyInput}
+      handleAdd={this.handleAdd}
+      handleSubmit={this.handleSubmit}
+    />;
+  }
 }
 
 const AddContractor = connect(
-    mapStateToProps,
-    null
+  mapStateToProps,
+  null
 )(AddContractorContainer);
 
 export default AddContractor;
