90c44ca7f599eb5cf85a0567060524d64d3bb046
vaastav <vaastav.anand05@gmail.com>
2017-11-13 17:20:50 -0800

    Add Edit/Add functionality to skills table

--- code/backend/src/main/java/server/rest/controllers/SkillsController.java
+++ code/backend/src/main/java/server/rest/controllers/SkillsController.java
@@ -2,15 +2,20 @@ package server.rest.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import server.database.DatabaseConnection;
 import server.model.Skill;
+import server.rest.responses.SkillsAddResponse;
+import server.rest.responses.SkillsEditResponse;
 import server.rest.responses.SkillsResponse;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,6 +23,8 @@ import java.util.logging.Logger;
 @RestController
 public class SkillsController extends Controller {
     private static String getQuery = "select * from Skill;";
+    private static String insertQuery = "insert into Skill values(?, ?, ?, ?);";
+    private static String updateQuery = "update Skill set name=?, description=?, type=? where id=?";
 
     @RequestMapping("/skills/view")
     public SkillsResponse skills() {
@@ -44,5 +51,65 @@ public class SkillsController extends Controller {
         }
 
         return new SkillsResponse(skills);
+    }
+
+    @RequestMapping("/skills/add")
+    public SkillsAddResponse addSkill(
+            @RequestParam("name") String name,
+            @RequestParam("description") String description,
+            @RequestParam("type") String type) {
+        DatabaseConnection connection = new DatabaseConnection(dbConnectionUrl, dbUsername, dbPassword);
+        int index = ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE);
+        Skill skill = new Skill(index, name, description, type);
+        try {
+            connection.openConnection();
+            if (!connection.isConnected()) {
+                return SkillsAddResponse.addSkillsFailure("Failed to connect to database");
+            }
+            PreparedStatement st = connection.getPreparedStatement(insertQuery);
+            int i = 1;
+            st.setInt(i++, skill.getId());
+            st.setString(i++, skill.getName());
+            st.setString(i++, skill.getDescription());
+            st.setString(i++, skill.getType());
+            int success = st.executeUpdate();
+            connection.commitTransaction();
+            if (success == 0) {
+                SkillsAddResponse.addSkillsFailure("Failed to add skill");
+            }
+        } catch (SQLException e) {
+            Logger logger = Logger.getAnonymousLogger();
+            logger.log(Level.INFO, "Add skill failed: " + e.getMessage());
+            return SkillsAddResponse.addSkillsFailure(e.getMessage());
+        }
+        return new SkillsAddResponse(skill);
+    }
+
+    @RequestMapping("/skills/edit")
+    public SkillsEditResponse editSkill(
+            @RequestParam("id") int id,
+            @RequestParam("name") String name,
+            @RequestParam("description") String description,
+            @RequestParam("type") String type) {
+        DatabaseConnection connection = new DatabaseConnection(dbConnectionUrl, dbUsername, dbPassword);
+        Skill skill = new Skill(id, name, description, type);
+        try {
+            connection.openConnection();
+            if (!connection.isConnected()) {
+                return SkillsEditResponse.editSkillFailure("Failed to connect to database");
+            }
+            PreparedStatement st = connection.getPreparedStatement(updateQuery);
+            int i = 1;
+            st.setString(i++, skill.getName());
+            st.setString(i++, skill.getDescription());
+            st.setString(i++, skill.getType());
+            st.setInt(i++, skill.getId());
+            st.executeQuery();
+        } catch (SQLException e) {
+            Logger logger = Logger.getAnonymousLogger();
+            logger.log(Level.INFO, "Edit skill failed: " + e.getMessage());
+            return SkillsEditResponse.editSkillFailure(e.getMessage());
+        }
+        return new SkillsEditResponse(skill);
     }
 }
---
+++ code/backend/src/main/java/server/rest/responses/SkillsEditResponse.java
@@ -0,0 +1,26 @@
+package server.rest.responses;
+
+import server.model.Skill;
+
+/**
+ * Created by vaast on 13/11/2017.
+ */
+public class SkillsEditResponse extends Response {
+    Skill skill;
+
+    public static SkillsEditResponse editSkillFailure(String msg) {
+        Skill skill = new Skill(0, "NA", "NA", "NA");
+        SkillsEditResponse response = new SkillsEditResponse(skill);
+        response.setError(true);
+        response.setErrorMessage(msg);
+        return response;
+    }
+
+    public SkillsEditResponse(Skill skill) {
+        this.skill = skill;
+    }
+
+    public Skill getSkill() {
+        return skill;
+    }
+}
