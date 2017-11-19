3bdfc1b8e2acd2fe9f1d53ad4a9e34661d447296
Shrey Swades Nayak <sswades@gmail.com>
2017-11-18 23:23:27 -0800

    Updated the front-end of edit contractor

---
+++ code/frontend/src/components/ContractorInfo/EditPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from '../AddContractor/addcontractor.css';
+import Contractor from '../AddContractor/Contractor.jsx';
+import Projects from '../AddContractor/Projects.jsx';
+
+function EditContractorComponent({contractor, projects, handleTextInput, handleDropdownInput,
+  handleDateInput, handleRadioInput, handleAdd, handleSubmit, message, tables}) {
+  return (
+    <div className={css.wrapper}>
+      { message === '' ? null : <p>{message}</p> }
+      <h1>Edit Contractor</h1>
+      <p> Use the form below to edit contractor information stored in the system.</p>
+      <Contractor contractor={contractor} handleTextInput={handleTextInput} handleRadioInput={handleRadioInput}/>
+      <Projects projects={projects} handleTextInput={handleTextInput} handleDropdownInput={handleDropdownInput}
+        handleDateInput={handleDateInput} handleRadioInput={handleRadioInput} tables={tables} />
+      <input className={css.btnstyle} type="submit" onClick={handleAdd} value="Add Additional Contract" />
+      <input className={css.btnstyle} type="submit" onClick={handleSubmit} value="Save Contractor" />
+    </div>
+  );
+}
+
+EditContractorComponent.propTypes = {
+  contractor: PropTypes.object.isRequired,
+  projects: PropTypes.array.isRequired,
+  handleTextInput: PropTypes.func.isRequired,
+  handleDropdownInput: PropTypes.func.isRequired,
+  handleDateInput: PropTypes.func.isRequired,
+  handleAdd: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleRadioInput: PropTypes.func.isRequired,
+  message: PropTypes.string.isRequired,
+  tables: PropTypes.object.isRequired
+};
+
+export default EditContractorComponent;
