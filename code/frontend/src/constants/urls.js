3d5dd922d4a56aeee865a570af976425b67b1af1
Maia Kamyab <mahtakamyab@gmail.com>
2017-11-28 15:43:09 -0800

    return localhost to live site

--- code/frontend/src/constants/urls.js
+++ code/frontend/src/constants/urls.js
@@ -1,2 +1,2 @@
-//export const LIVE_SITE = 'http://theterminal-env.us-west-2.elasticbeanstalk.com/';
-export const LIVE_SITE = 'http://localhost:8080/';
+export const LIVE_SITE = 'http://theterminal-env.us-west-2.elasticbeanstalk.com/';
+export const LOCALHOST = 'http://localhost:8080/';
