f77ab313b2a6dbf6ad04bf26c861607445d3ab8e
Maia Kamyab <mahtakamyab@gmail.com>
2017-11-28 22:31:14 -0800

    login tests

--- code/frontend/test/login-actions-test.js
+++ code/frontend/test/login-actions-test.js
@@ -2,11 +2,26 @@ import { expect } from 'chai';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import nock from 'nock';
-import { loginUser } from '../src/actions/login-actions.js';
+import { loginUser, validateSession, logout } from '../src/actions/login-actions.js';
 import * as TYPES from '../src/constants/action-types.js';
+import { LIVE_SITE } from '../src/constants/urls.js';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
+
+const mockSetFunction = {};
+mockSetFunction.set = () => expect(true).to.be.true;
+mockSetFunction.remove = () => expect.fail('Wrong function was called');
+
+const mockRemoveFunction = {};
+mockRemoveFunction.remove = () => expect(true).to.be.true;
+mockRemoveFunction.set = () => expect.fail('Wrong function was called');
+
+/* ================================================
+*
+* LOGIN
+*
+* */
 
 describe('Login user API call', () => {
   let store;
@@ -17,7 +32,8 @@ describe('Login user API call', () => {
         isLoggedIn: false,
         isAdmin: false,
         username: '',
-        error: false
+        error: false,
+        token: ''
       }
     });
   });
@@ -27,7 +43,7 @@ describe('Login user API call', () => {
   });
 
   it('Should return correct response on successful call - normal user permissions', () => {
-    nock('https://localhost:8443')
+    nock(LIVE_SITE)
       .get('/login')
       .query({
         username: 'user',
@@ -39,7 +55,8 @@ describe('Login user API call', () => {
         'errorMessage': null,
         'loginSuccessful': true,
         'username': 'user',
-        'permissions': 'write'
+        'permissions': 'write',
+        'token': 'user'
       });
 
     const expectedActions = [
@@ -54,11 +71,12 @@ describe('Login user API call', () => {
       {
         type: TYPES.LOGIN,
         username: 'user',
-        isAdmin: false
+        isAdmin: false,
+        token: 'user'
       }
     ];
 
-    return store.dispatch(loginUser('user', 'user')).then(() => {
+    return store.dispatch(loginUser('user', 'user', mockSetFunction)).then(() => {
       expect(store.getActions()).to.deep.equal(expectedActions);
     }).catch((err) => {
       expect.fail(err);
@@ -66,7 +84,7 @@ describe('Login user API call', () => {
   });
 
   it('Should return correct response on successful call - admin user permissions', () => {
-    nock('https://localhost:8443')
+    nock(LIVE_SITE)
       .get('/login')
       .query({
         username: 'admin',
@@ -78,7 +96,8 @@ describe('Login user API call', () => {
         'errorMessage': null,
         'loginSuccessful': true,
         'username': 'admin',
-        'permissions': 'admin'
+        'permissions': 'admin',
+        'token': 'admin'
       });
 
     const expectedActions = [
@@ -93,11 +112,12 @@ describe('Login user API call', () => {
       {
         type: TYPES.LOGIN,
         username: 'admin',
-        isAdmin: true
+        isAdmin: true,
+        token: 'admin'
       }
     ];
 
-    return store.dispatch(loginUser('admin', 'admin')).then(() => {
+    return store.dispatch(loginUser('admin', 'admin', mockSetFunction)).then(() => {
       expect(store.getActions()).to.deep.equal(expectedActions);
     }).catch((err) => {
       expect.fail(err);
@@ -107,7 +127,7 @@ describe('Login user API call', () => {
   it('Should return correct response on unsuccessful call - error message in body', () => {
     const errorMessage = 'test error message';
 
-    nock('https://localhost:8443')
+    nock(LIVE_SITE)
       .get('/login')
       .query({
         username: 'admin',
@@ -119,7 +139,8 @@ describe('Login user API call', () => {
         'errorMessage': errorMessage,
         'loginSuccessful': false,
         'username': '',
-        'permissions': 'none'
+        'permissions': 'none',
+        'token': ''
       });
 
     const expectedActions = [
@@ -137,7 +158,7 @@ describe('Login user API call', () => {
       }
     ];
 
-    return store.dispatch(loginUser('admin', '1234')).then(() => {
+    return store.dispatch(loginUser('admin', '1234', mockRemoveFunction)).then(() => {
       expect.fail('This should have failed');
     }).catch(() => {
       expect(store.getActions()).to.deep.equal(expectedActions);
@@ -145,7 +166,7 @@ describe('Login user API call', () => {
   });
 
   it('Should return correct response on unsuccessful call - error in request', () => {
-    nock('https://localhost:8443')
+    nock(LIVE_SITE)
       .get('/login')
       .query({
         username: 'admin',
@@ -157,7 +178,8 @@ describe('Login user API call', () => {
         'errorMessage': 'test error message',
         'loginSuccessful': false,
         'username': '',
-        'permissions': 'none'
+        'permissions': 'none',
+        'token': ''
       });
 
     const expectedActions = [
@@ -175,8 +197,240 @@ describe('Login user API call', () => {
       }
     ];
 
-    return store.dispatch(loginUser('admin', '1234')).then(() => {
+    return store.dispatch(loginUser('admin', '1234', mockRemoveFunction)).then(() => {
       expect.fail('This should have failed');
+    }).catch(() => {
+      expect(store.getActions()).to.deep.equal(expectedActions);
+    });
+  });
+});
+
+/* ================================================
+*
+* REFRESH
+*
+* */
+
+describe('Refresh session API call', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      user: {
+        isLoggedIn: false,
+        isAdmin: false,
+        username: '',
+        error: false,
+        token: ''
+      }
+    });
+  });
+
+  afterEach(() => {
+    store = null;
+  });
+
+  it('Should return correct response on successful call - normal user permissions', () => {
+    nock(LIVE_SITE)
+      .get('/refresh')
+      .query({
+        username: 'user',
+        token: 'user'
+      })
+      .delay(0)
+      .reply(200, {
+        'error': false,
+        'errorMessage': null,
+        'permissions': 'write'
+      });
+    const expectedActions = [
+      {
+        type: TYPES.TOGGLE_LOADING,
+        isLoading: true
+      },
+      {
+        type: TYPES.TOGGLE_LOADING,
+        isLoading: false
+      },
+      {
+        type: TYPES.LOGIN,
+        username: 'user',
+        isAdmin: false,
+        token: 'user'
+      }
+    ];
+
+    return store.dispatch(validateSession('user', 'user', mockSetFunction)).then(() => {
+      expect(store.getActions()).to.deep.equal(expectedActions);
+    }).catch((err) => {
+      expect.fail(err);
+    });
+  });
+
+  it('Should return correct response on successful call - admin user permissions', () => {
+    nock(LIVE_SITE)
+      .get('/refresh')
+      .query({
+        username: 'user',
+        token: 'user'
+      })
+      .delay(0)
+      .reply(200, {
+        'error': false,
+        'errorMessage': null,
+        'permissions': 'admin'
+      });
+    const expectedActions = [
+      {
+        type: TYPES.TOGGLE_LOADING,
+        isLoading: true
+      },
+      {
+        type: TYPES.TOGGLE_LOADING,
+        isLoading: false
+      },
+      {
+        type: TYPES.LOGIN,
+        username: 'user',
+        isAdmin: true,
+        token: 'user'
+      }
+    ];
+
+    return store.dispatch(validateSession('user', 'user', mockSetFunction)).then(() => {
+      expect(store.getActions()).to.deep.equal(expectedActions);
+    }).catch((err) => {
+      expect.fail(err);
+    });
+  });
+
+  it('Should return correct response on failed call - bad request', () => {
+    nock(LIVE_SITE)
+      .get('/refresh')
+      .query({
+        username: 'user',
+        token: 'user'
+      })
+      .delay(0)
+      .reply(400, {
+        'error': true,
+        'errorMessage': 'Bad Request',
+        'permissions': ''
+      });
+    const expectedActions = [
+      {
+        type: TYPES.TOGGLE_LOADING,
+        isLoading: true
+      },
+      {
+        type: TYPES.TOGGLE_LOADING,
+        isLoading: false
+      },
+      {
+        type: TYPES.LOGIN_FAILED,
+        error: 'Bad Request - Please sign in again.'
+      }
+    ];
+
+    return store.dispatch(validateSession('user', 'user', mockRemoveFunction)).then(() => {
+      expect.fail('Call should have failed');
+    }).catch(() => {
+      expect(store.getActions()).to.deep.equal(expectedActions);
+    });
+  });
+});
+
+/* ================================================
+*
+* LOGOUT
+*
+* */
+
+describe('Logout user API call', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      user: {
+        isLoggedIn: false,
+        isAdmin: false,
+        username: '',
+        error: false,
+        token: ''
+      }
+    });
+  });
+
+  afterEach(() => {
+    store = null;
+  });
+
+  it('Should return correct response on successful call', () => {
+    nock(LIVE_SITE)
+      .get('/logout')
+      .query({
+        username: 'user',
+        token: 'user'
+      })
+      .delay(0)
+      .reply(200, {
+        'error': false,
+        'errorMessage': null
+      });
+    const expectedActions = [
+      {
+        type: TYPES.TOGGLE_LOADING,
+        isLoading: true
+      },
+      {
+        type: TYPES.TOGGLE_LOADING,
+        isLoading: false
+      },
+      {
+        type: TYPES.LOGOUT
+      }
+    ];
+
+    return store.dispatch(logout('user', 'user')).then(() => {
+      expect(store.getActions()).to.deep.equal(expectedActions);
+    }).catch((err) => {
+      expect.fail(err);
+    });
+  });
+
+  it('Should return correct response on failed call - bad request', () => {
+    nock(LIVE_SITE)
+      .get('/logout')
+      .query({
+        username: 'user',
+        token: 'user'
+      })
+      .delay(0)
+      .reply(400, {
+        'error': true,
+        'errorMessage': 'Bad Request',
+        'permissions': ''
+      });
+    const expectedActions = [
+      {
+        type: TYPES.TOGGLE_LOADING,
+        isLoading: true
+      },
+      {
+        type: TYPES.TOGGLE_LOADING,
+        isLoading: false
+      },
+      {
+        type: TYPES.LOGOUT
+      },
+      {
+        type: TYPES.LOGIN_FAILED,
+        error: 'Failed to remove session from backend: Bad Request'
+      }
+    ];
+
+    return store.dispatch(logout('user', 'user')).then(() => {
+      expect.fail('Call should have failed');
     }).catch(() => {
       expect(store.getActions()).to.deep.equal(expectedActions);
     });
