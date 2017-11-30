4bbb594406c80bddfaa63e9d74c38e808020b79c
Anushka Agrawal <agrawalanushka93@gmail.com>
2017-11-29 22:18:53 -0800

    selenium test for login and dashboard done

--- code/frontend/test-selenium/selenium.js
+++ code/frontend/test-selenium/selenium.js
@@ -20,19 +20,56 @@ module.exports = {
         this.assert.equal(comp.value, 'Dashboard')
     })
   },
+
   'dashboard buttons': (browser) => {
     browser
       // click the data filtering button to navigate to that page
-    .click('button[data-qa="datafilterbtn"]')
+      .click('button[data-qa="datafilterbtn"]')
       // wait for the data filtering page to load
-    .waitforElementVisible('div[data-qa="filtering-wrapper"]', 3000)
+      .waitForElementVisible('div[data-qa="filtering-wrapper"]', 3000)
       // check if page loaded was the filtering page
-    .getText('h1[data-qa="filtering-header"]', function(comp) {
+      .getText('h1[data-qa="filtering-header"]', function(comp) {
         this.assert.equal(comp.value, 'Data Filtering System')
       })
-      .click()
+      // go back to dashboard
+      .click('li[data-qa="dashboard-nav"]')
 
+      // click the contractor information button to navigate to that page
+      .click('button[data-qa="infobtn"]')
+      // wait for the data filtering page to load
+      .waitForElementVisible('div[data-qa=info-wrapper]', 3000)
+      // check if page loaded was the information page
+      .getText('h1[data-qa="info-header"]', function(comp) {
+        this.assert.equal(comp.value, 'Contractor Information')
+      })
+      // go back to dashboard
+      .click('li[data-qa="dashboard-nav"]')
 
+      // click the reports button to navigate to that page
+      .click('button[data-qa="reportsbtn"]')
+      // wait for the reports page to load
+      .waitForElementVisible('div[data-qa="reports-wrapper"]', 3000)
+      // check if page loaded was the filtering page
+      .getText('h1[data-qa="reports-header"]', function(comp) {
+        this.assert.equal(comp.value, 'Trending Reports')
+      })
+      // go back to dashboard
+      .click('li[data-qa="dashboard-nav"]')
 
-  }
+      //click the add contractor button to navigate to that page
+      .click('button[data-qa="addbtn"]')
+      // wait for the add contractor page to load
+      .waitForElementVisible('div[data-qa="add-wrapper"]', 3000)
+      // check if page loaded was the add contractor page
+      .getText('h1[data-qa="add-header"]', function(comp) {
+        this.assert.equal(comp.value, 'Add Contractor')
+      })
+  },
+
+  'add contractors': (browser) => {
+    browser
+      .setValue('input[data-qa="loginInput"]', "admin")
+
+}
+
 };
